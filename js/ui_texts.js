e8c11af4d9fca6450ff527d172b3473f6640bc7b
Fichtmüller <d.fichtmueller@bgbm.org>
2025-07-23 18:35:14 +0200

    initial commit
    still a lot of work to do

---
+++ js/ui_texts.js
@@ -0,0 +1,146 @@
+var ui_texts = {
+	"countries" : {
+		"global": {
+			"name": "Global",
+			"flag": "🌍"
+		},
+		"europe": {
+			"name": "Europe",
+			"flag": "🇪🇺"
+		},
+		"at": {
+			"name": "Austria",
+			"flag": "🇦🇹"
+		},
+		"be": {
+			"name": "Belgium",
+			"flag": "🇧🇪"
+		},
+		"bg": {
+			"name": "Bulgaria",
+			"flag": "🇧🇬"
+		},
+		"hr": {
+			"name": "Croatia",
+			"flag": "🇭🇷"
+		},
+		"cz": {
+			"name": "Czech Republic",
+			"flag": "🇨🇿"
+		},
+		"cy": {
+			"name": "Cyprus",
+			"flag": "🇨🇾"
+		},
+		"dk": {
+			"name": "Denmark",
+			"flag": "🇩🇰"
+		},
+		"ee": {
+			"name": "Estonia",
+			"flag": "🇪🇪"
+		},
+		"fi": {
+			"name": "Finland",
+			"flag": "🇫🇮"
+		},
+		"fr": {
+			"name": "France",
+			"flag": "🇫🇷"
+		},
+		"de": {
+			"name": "Germany",
+			"flag": "🇩🇪"
+		},
+		"gr": {
+			"name": "Greece",
+			"flag": "🇬🇷"
+		},
+		"hu": {
+			"name": "Hungary",
+			"flag": "🇭🇺"
+		},
+		"is": {
+			"name": "Iceland",
+			"flag": "🇮🇸"
+		},
+		"ie": {
+			"name": "Ireland",
+			"flag": "🇮🇪"
+		},
+		"it": {
+			"name": "Italy",
+			"flag": "🇮🇹"
+		},
+		"lv": {
+			"name": "Latvia",
+			"flag": "🇱🇻"
+		},
+		"lt": {
+			"name": "Lithuania",
+			"flag": "🇱🇹"
+		},
+		"lu": {
+			"name": "Luxembourg",
+			"flag": "🇱🇺"
+		},
+		"mt": {
+			"name": "Malta",
+			"flag": "🇲🇹"
+		},
+		"nl": {
+			"name": "Netherlands",
+			"flag": "🇳🇱"
+		},
+		"no": {
+			"name": "Norway",
+			"flag": "🇳🇴"
+		},
+		"pl": {
+			"name": "Poland",
+			"flag": "🇵🇱"
+		},
+		"pt": {
+			"name": "Portugal",
+			"flag": "🇵🇹"
+		},
+		"ro": {
+			"name": "Romania",
+			"flag": "🇷🇴"
+		},
+		"sk": {
+			"name": "Slovakia",
+			"flag": "🇸🇰"
+		},
+		"si": {
+			"name": "Slovenia",
+			"flag": "🇸🇮"
+		},
+		"es": {
+			"name": "Spain",
+			"flag": "🇪🇸"
+		},
+		"se": {
+			"name": "Sweden",
+			"flag": "🇸🇪"
+		},
+		"ch": {
+			"name": "Switzerland",
+			"flag": "🇨🇭"
+		},
+		"gb": {
+			"name": "United Kingdom",
+			"flag": "🇬🇧"
+		}
+	},
+	"purpose": {
+		"canonical_name_checked": "Canonical Name Checked",
+		"full_name_checked": "Full Name Checked",
+		"classification": "Classification",
+		"acceptance": "Acceptance",
+	},
+	"requirements": {
+		"asdf": "ASDF"
+	}
+	
+}
